0981fefc6255a7f2ac703b99b3094b83b5fa8ec6
Your Name <you@example.com>
2024-10-28 16:55:31 -0400

    cafecitslol

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Script is loaded")
+const hambergure = document.getElementById("hambergure")
+const header = document.querySelector("header")
+function open() {
+    hambergure.classList.toggle("change");
+    header.classList.toggle("open");
+}
+
+hambergure.addEventListener("click", open);
+
+const links = document.querySelectorAll(".nav-link");
+ links.forEach((link) => {
+    link.addEventListener("click", open);
+ });
